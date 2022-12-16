340234948d3b1ff591e4ffc775fc983350b6fee2
Tinashe Chikomo <46610714+hybridadmin@users.noreply.github.com>
2022-12-16 12:27:24 +0200

    Update install_docker.sh

--- wsl/install_docker.sh
+++ wsl/install_docker.sh
@@ -10,9 +10,9 @@ fi
 
 # docker-compose
 if [ ! -f "/usr/local/bin/docker-compose" ]; then
-LATEST_VERSION_LINK=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep -E "\"browser_download_url.+linux-x86_64\"" | cut -d '"' -f4)
-sudo curl -L "$LATEST_VERSION_LINK" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+	LATEST_VERSION_LINK=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep -E "\"browser_download_url.+linux-x86_64\"" | cut -d '"' -f4)
+	sudo curl -L "$LATEST_VERSION_LINK" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 # reload docker group
